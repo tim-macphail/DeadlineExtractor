2847bce92a8b60e87380899fbf117636ef9b9169
Tim Macphail <timothy.macphail@gmail.com>
2025-08-24 22:23:43 -0600

    mock llm response

---
+++ backend/llm.py
@@ -0,0 +1,7 @@
+def search_for_deadlines(text: str):
+    return [{
+        "sourceText": "Assignment 5 is due on October 10th 2023",
+        "name": "Assignment 5",
+        "date": "2023-10-10",
+        "description": "" # description is optional
+    }]
